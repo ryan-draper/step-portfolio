ac2c0dee38565cf3a59929e342e67dc1bd922f86
Ryan Draper <draperryanm@gmail.com>
2020-06-03 22:51:40 +0000

    Remove unecessary commented code from script.js and DataServlet.java.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -26,14 +26,7 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  // Old doGet method for Hello World content output:
-  /**
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Ryan!</h1>");
-  }
-  */
+
   private List<String> quotes;
   
   @Override
@@ -46,17 +39,6 @@ public class DataServlet extends HttpServlet {
     quotes.add("\"Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.\"");
     quotes.add("\"Kids, you tried your best and you failed miserably. The lesson is never try.\"");
   }
-  
-  // Old doGet method for choosing a random quote and displaying it
-  /**
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String quote = quotes.get((int) (Math.random() * quotes.size()));
-
-    response.setContentType("text/html;");
-    response.getWriter().println(quote);
-  }
-  */
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,35 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// /**
-//  * Adds a random quote to the page.
-//  */
-// function addRandomQuote() {
-// const quotes =
-//       ['\"There\'s only one thing I hate more than lying: skim milk. Which is water lying about being milk.\" - Ron Swanson', 
-//       '\"Just remember, every time you look up at the moon, I, too, will be looking at a moon. Not the same one, obviously. That\'s impossible.\" - Andy Dwyer',
-//       '\"I\'ve made a huge mistake.\" - Gob Bluth', '\"There\'s always money in the banana stand.\" - George Bluth',
-//       '\"Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.\" - Michael Scott',
-//        '\"Kids, you tried your best and you failed miserably. The lesson is never try.\" - Homer Simpson'];
-
-//   // Pick a random quote.
-//   const quote = quotes[Math.floor(Math.random() * quotes.length)];
-
-//   // Add it to the page.
-//   const quoteContainer = document.getElementById('quote-container');
-//   quoteContainer.innerText = quote;
-// }
-
-// /**
-//  * Adds a random quote to the page using fetch() to request content from the server.
-//  */
-// function getRandomQuoteUsingArrowFunctions() {
-//   fetch('/data').then(response => response.text()).then((quote) => {
-//     console.log('Adding quote to dom: ' + quote);
-//     document.getElementById('quote-container').innerText = quote;
-//   });
-// }
-
 /**
  * Fetches stats from the servers and adds them to the DOM.
  */
@@ -72,4 +43,3 @@ function createListElement(text) {
   liElement.innerText = text;
   return liElement;
 }
-
