7a3cadb6fad4e34b0d22bfe297f698f62e834283
Ryan Draper <draperryanm@gmail.com>
2020-06-09 01:14:53 +0000

    Add a drop down menu to select the number of comments displayed (updates without full page refresh).

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,11 +15,34 @@
 
 /** Fetches comments from the server and adds them to the DOM. */
 function loadComments() {
-  console.log("loadComments() is running");
-  fetch('/data').then(response => response.json()).then((comments) => {
+  // loads 5 comments by default
+  fetch('/data?number-comments=5').then(response => response.json()).then((comments) => {
     const commentListElement = document.getElementById('comment-list');
+    var numComments = 5;
+    var i = 0;
     comments.forEach((comment) => {
-      commentListElement.appendChild(createCommentElement(comment));
+      if (i < numComments) {
+        commentListElement.appendChild(createCommentElement(comment));
+        i++;
+      }
+    })
+  });
+}
+
+/** Updates number of comments shown based on drop-down menu. */
+function updateComments() {
+  // clear out old comments
+  document.getElementById("comment-list").innerHTML = "";
+  // reads in selection from drop-down menu for number of comments
+  var numComments = document.getElementById("number-comments").value;
+  fetch('/data?number-comments=' + numComments.toString()).then(response => response.json()).then((comments) => {
+    const commentListElement = document.getElementById('comment-list');
+    var i = 0;
+    comments.forEach((comment) => {
+      if (i < numComments) {
+        commentListElement.appendChild(createCommentElement(comment));
+        i++;
+      }
     })
   });
 }
